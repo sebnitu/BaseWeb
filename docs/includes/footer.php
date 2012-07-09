b76fe5078b50d1066bf2e0c14c22b521ac0f95b1
Sebastian Nitu <me@sebnitu.com>
2012-07-09 15:03:00 -0700

    not sure what I'm doing

---
+++ docs/includes/footer.php
@@ -0,0 +1,38 @@
+	</div><!-- .container -->
+</section><!-- .baseWeb-demo -->
+
+<footer class="footer">
+	<div class="container">
+		<div class="grid-12">
+			<p>BaseWeb is built and maintained by Sebastian Nitu.<br>&copy; Copyright 2012, Sebastian Nitu. All rights reserved.</p>
+		</div>
+	</div><!-- .container -->
+</footer>
+
+<!-- jQuery -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+<script>!window.jQuery && document.write(unescape('%3Cscript src="assets/js/jquery.min.js"%3E%3C/script%3E'))</script>
+
+<!-- Google's Code Prettify -->
+<script src="assets/js/prettify.js"></script>
+<script>prettyPrint();</script>
+
+<!-- jQuery functions, document ready & window load -->
+<script src="assets/js/jquery.function.js"></script>
+<script src="assets/js/jquery.docready.js"></script>
+
+<!-- Asynchronous Google Analytics: mathiasbynens.be/notes/async-analytics-snippet
+     change the UA-XXXXX-X to be your site's ID -->
+<script>
+ var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
+ (function(d, t) {
+  var g = d.createElement(t),
+      s = d.getElementsByTagName(t)[0];
+  g.async = true;
+  g.src = ('https:' == location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  s.parentNode.insertBefore(g, s);
+ })(document, 'script');
+</script>
+
+</body>
+</html>
---
+++ docs/includes/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="ie9 ie8 ie7 ie6" lang="en"><![endif]-->
+<!--[if IE 7]><html class="ie9 ie8 ie7" lang="en"><![endif]-->
+<!--[if IE 8]><html class="ie9 ie8" lang="en"><![endif]-->
+<!--[if IE 9]><html class="ie9" lang="en"><![endif]-->
+<!--[if gt IE 9]><!--><html lang="en"><!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title></title>
+	<meta name="description" content="">
+	
+	<!-- Stylesheets and less -->
+	<link rel="stylesheet/less" type="text/css" media="all" href="assets/css/less/bootstrap.less">
+	<script src="assets/js/less.js"></script>
+	<script>
+		less.env = "development";
+		less.watch();
+		localStorage.clear();
+	</script>
+	
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
+	<link rel="shortcut icon" href="assets/img/layout/favicon.png">
+	<link rel="apple-touch-icon" href="assets/img/layout/apple-touch-icon-57.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/img/layout/apple-touch-icon-72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/img/layout/apple-touch-icon-114.png">
+	
+</head>
+<body>
+
+<header class="header">
+	<div class="container">
+		
+		<div class="grid-12">
+			<h1 class="logo">
+				<a href="#"><strong>Base</strong>Web</a>
+			</h1>
+		</div>
+		
+	</div><!-- .container -->
+</header><!-- .header -->
+
+<section class="baseWeb-demo">
+	<div class="container">
