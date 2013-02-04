34ed4ab3c989a70cf20c7fbf9d0bfd707d1e9510
Sebastian Nitu <me@sebnitu.com>
2013-02-04 11:02:02 -0800

    Removed browser prefixes for border radius and removed boxshadow all together from mixins. Also added different variations in the way you can write the border radius mixin

--- docs/includes/header.php
+++ docs/includes/header.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title></title>
+    <title>BaseWeb</title>
     <meta name="description" content="">
     
     <!-- Stylesheets and less -->
