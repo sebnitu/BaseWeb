9b20dd9222a1ebe1ef712a73faae72f113403fec
Sebastian Nitu <me@sebnitu.com>
2013-03-20 01:11:54 -0700

    Updated some of the doc styling for grid system

--- docs/grid.php
+++ docs/grid.php
@@ -12,11 +12,60 @@
 
 <div class="demo demo-grid">
     
-    <?php /*
-    <div class="row nested nested-static">
-        
-        <div class="col-12"><h3>Nested Static</h3></div>
-        
+    <div class="semantic">
+        <div class="test-1">
+            <p>.column(3);</p>
+        </div>
+        <div class="test-2">
+            <p>.column(3);</p>
+        </div>
+        <div class="test-3">
+            <p>.column(3); .prefix(3); .suffix(6);</p>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-12"><p>.col-12</p></div>
+    </div><!-- .row -->
+    
+    <div class="row">
+        <div class="col-1"><p>.</p></div>
+        <div class="col-11"><p>.col-11</p></div>
+    </div><!-- .row -->
+    
+    <div class="row">
+        <div class="col-2"><p>.col-2</p></div>
+        <div class="col-10"><p>.col-10</p></div>
+    </div><!-- .row -->
+    
+    <div class="row">
+        <div class="col-3"><p>.col-3</p></div>
+        <div class="col-9"><p>.col-9</p></div>
+    </div><!-- .row -->
+    
+    <div class="row">
+        <div class="col-4"><p>.col-4</p></div>
+        <div class="col-8"><p>.col-8</p></div>
+    </div><!-- .row -->
+    
+    <div class="row">
+        <div class="col-5"><p>.col-5</p></div>
+        <div class="col-7"><p>.col-7</p></div>
+    </div><!-- .row -->
+    
+    <div class="row">
+        <div class="col-6"><p>.col-6</p></div>
+        <div class="col-6"><p>.col-6</p></div>
+    </div><!-- .row -->
+    
+    <div class="row">
+        <div class="col-4 prefix-2 suffix-6"><p>.col-4 .prefix-2 .suffix-6</p></div>
+        <div class="col-4 prefix-6 suffix-2"><p>.col-4 .prefix-6 .suffix-2</p></div>
+
+    </div><!-- .row -->
+    
+        <div class="row nested nested-static hidden-tablet">
+                
         <div class="col-8">
             <p>.col-8</p>
             
@@ -79,13 +128,9 @@
         </div>
         
     </div><!-- .row -->
-    */ ?>
     
-    <?php /*
-    <div class="row nested nested-fluid">
-        
-        <div class="col-12"><h3>Nested Fluid</h3></div>
-        
+    <div class="row nested nested-fluid visible-tablet">
+                
         <div class="col-8">
             <p>.col-8</p>
             
@@ -148,58 +193,6 @@
         </div>
         
     </div><!-- .row -->
-    */ ?>
-    
-    <div class="semantic">
-        <div class="test-1">
-            <p>Test</p>
-        </div>
-        <div class="test-2">
-            <p>Test</p>
-        </div>
-        <div class="test-3">
-            <p>Test</p>
-        </div>
-    </div>
-    
-    <div class="row">
-        <div class="col-12"><p>.col-12</p></div>
-    </div><!-- .row -->
-    
-    <div class="row">
-        <div class="col-1"><p>.</p></div>
-        <div class="col-11"><p>.col-11</p></div>
-    </div><!-- .row -->
-    
-    <div class="row">
-        <div class="col-2"><p>.col-2</p></div>
-        <div class="col-10"><p>.col-10</p></div>
-    </div><!-- .row -->
-    
-    <div class="row">
-        <div class="col-3"><p>.col-3</p></div>
-        <div class="col-9"><p>.col-9</p></div>
-    </div><!-- .row -->
-    
-    <div class="row">
-        <div class="col-4"><p>.col-4</p></div>
-        <div class="col-8"><p>.col-8</p></div>
-    </div><!-- .row -->
-    
-    <div class="row">
-        <div class="col-5"><p>.col-5</p></div>
-        <div class="col-7"><p>.col-7</p></div>
-    </div><!-- .row -->
-    
-    <div class="row">
-        <div class="col-6"><p>.col-6</p></div>
-        <div class="col-6"><p>.col-6</p></div>
-    </div><!-- .row -->
-    
-    <div class="row">
-        <div class="col-4 prefix-2 suffix-6"><p>.col-4 .prefix-2 .suffix-6</p></div>
-        <div class="col-4 prefix-6 suffix-2"><p>.col-4 .prefix-6 .suffix-2</p></div>
 
-    </div><!-- .row -->
     
 </div><!-- .demo -->
