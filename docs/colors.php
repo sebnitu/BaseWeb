b37042cad769fa9fa2be06a053f0088668075736
Sebastian Nitu <me@sebnitu.com>
2013-03-22 14:04:06 -0700

    Updated colors documentation

--- docs/colors.php
+++ docs/colors.php
@@ -1,3 +1,131 @@
+<?php
+////
+// Color Palette Arrays
+////
+// Palette
+$palette = array(
+    
+    '@redLighter',
+    '@redLight',
+    '@red',
+    '@redDark',
+    '@redDarker',
+    
+    '@redOrangeLighter',
+    '@redOrangeLight',
+    '@redOrange',
+    '@redOrangeDark',
+    '@redOrangeDarker',
+    
+    '@orangeLighter',
+    '@orangeLight',
+    '@orange',
+    '@orangeDark',
+    '@orangeDarker',
+    
+    '@yellowOrangeLighter',
+    '@yellowOrangeLight',
+    '@yellowOrange',
+    '@yellowOrangeDark',
+    '@yellowOrangeDarker',
+    
+    '@yellowLighter',
+    '@yellowLight',
+    '@yellow',
+    '@yellowDark',
+    '@yellowDarker',
+    
+    '@yellowGreenLighter',
+    '@yellowGreenLight',
+    '@yellowGreen',
+    '@yellowGreenDark',
+    '@yellowGreenDarker',
+    
+    '@greenLighter',
+    '@greenLight',
+    '@green',
+    '@greenDark',
+    '@greenDarker',
+    
+    '@blueGreenLighter',
+    '@blueGreenLight',
+    '@blueGreen',
+    '@blueGreenDark',
+    '@blueGreenDarker',
+    
+    '@blueLighter',
+    '@blueLight',
+    '@blue',
+    '@blueDark',
+    '@blueDarker',
+    
+    '@blueVioletLighter',
+    '@blueVioletLight',
+    '@blueViolet',
+    '@blueVioletDark',
+    '@blueVioletDarker',
+    
+    '@violetLighter',
+    '@violetLight',
+    '@violet',
+    '@violetDark',
+    '@violetDarker',
+    
+    '@redVioletLighter',
+    '@redVioletLight',
+    '@redViolet',
+    '@redVioletDark',
+    '@redVioletDarker'
+    
+);
+////
+// Gray Scale
+$gray_scale = array(
+    '@g0',
+    '@g1',
+    '@g2',
+    '@g3',
+    '@g4',
+    '@g5',
+    '@g6',
+    '@g7',
+    '@g8',
+    '@g9',
+    '@gA',
+    '@gB',
+    '@gC',
+    '@gD',
+    '@gE',
+    '@gF'
+);
+////
+// Readable Gray Scale
+$readable_gray_scale = array(
+    '@black',
+    '@grayDarker',
+    '@grayDark',
+    '@gray',
+    '@grayLight',
+    '@grayLighter',
+    '@white'
+);
+////
+// RGB
+$rgb = array(
+    '@Rgb',
+    '@rGb',
+    '@rgB'
+);
+////
+// CMYK
+$cmyk = array(
+    '@Cmyk',
+    '@cMyk',
+    '@cmYk',
+    '@cmyK'
+);
+?>
+
 <div class="demo-intro">
     <header class="demo-header">
         <h1>Color Palette</h1>
@@ -5,115 +133,60 @@
 </div><!-- .demo-intro -->
 
 <div class="demo">
-        
-    <ul class="color-palletes theme">
-        <li class="c1"></li>
-        <li class="c2"></li>
-        <li class="c3"></li>
-        <li class="c4"></li>
-        <li class="c5"></li>
-        <li class="c6"></li>
-        <li class="c7"></li>
-        <li class="c8"></li>
-        <li class="c9"></li>
-        <li class="c10"></li>
-        
-        <li class="c11"></li>
-        <li class="c12"></li>
-        <li class="c13"></li>
-        <li class="c14"></li>
-        <li class="c15"></li>
-        <li class="c16"></li>
-        <li class="c17"></li>
-        <li class="c18"></li>
-        <li class="c19"></li>
-        <li class="c20"></li>
-        
-        <li class="c21"></li>
-        <li class="c22"></li>
-        <li class="c23"></li>
-        <li class="c24"></li>
-        <li class="c25"></li>
-        <li class="c26"></li>
-        <li class="c27"></li>
-        <li class="c28"></li>
-        <li class="c29"></li>
-        <li class="c30"></li>
-        
-        <li class="c31"></li>
-        <li class="c32"></li>
-        <li class="c33"></li>
-        <li class="c34"></li>
-        <li class="c35"></li>
-        <li class="c36"></li>
-        <li class="c37"></li>
-        <li class="c38"></li>
-        <li class="c39"></li>
-        <li class="c40"></li>
-        
-        <li class="c41"></li>
-        <li class="c42"></li>
-        <li class="c43"></li>
-        <li class="c44"></li>
-        <li class="c45"></li>
-        <li class="c46"></li>
-        <li class="c47"></li>
-        <li class="c48"></li>
-        <li class="c49"></li>
-        <li class="c50"></li>
-        
-        <li class="c51"></li>
-        <li class="c52"></li>
-        <li class="c53"></li>
-        <li class="c54"></li>
-        <li class="c55"></li>
-        <li class="c56"></li>
-        <li class="c57"></li>
-        <li class="c58"></li>
-        <li class="c59"></li>
-        <li class="c60"></li>
-    </ul>
     
-    <ul class="color-palletes gray-scale">
-        <li class="c1"></li>
-        <li class="c2"></li>
-        <li class="c3"></li>
-        <li class="c4"></li>
-        <li class="c5"></li>
-        <li class="c6"></li>
-        <li class="c7"></li>
-        <li class="c8"></li>
-        <li class="c9"></li>
-        <li class="c10"></li>
-        <li class="c11"></li>
-        <li class="c12"></li>
-        <li class="c13"></li>
-        <li class="c14"></li>
-        <li class="c15"></li>
-        <li class="c16"></li>
-    </ul>
+    <div class="demo-description">
+        <h2>Palette Theme Variables</h2>
+    </div>
+    <div class="demo-block">
+        <ul class="color-palletes theme">
+            <?php foreach ($palette as $key=>$color) {
+                echo '<li title="'.$color.'" class="c'.($key + 1).'"><p>'.$color.'</p></li>';
+            } ?>
+        </ul>
+    </div>
     
-    <ul class="color-palletes short-code-gray-scale">
-        <li class="c1"></li>
-        <li class="c2"></li>
-        <li class="c3"></li>
-        <li class="c4"></li>
-        <li class="c5"></li>
-        <li class="c6"></li>
-        <li class="c7"></li>
-    </ul>
+    <div class="demo-description">
+        <h2>Gray Scale Variables</h2>
+    </div>
+    <div class="demo-block">
+        <ul class="color-palletes gray-scale">
+            <?php foreach ($gray_scale as $key=>$color) {
+                echo '<li title="'.$color.'" class="c'.($key + 1).'"><p>'.$color.'</p></li>';
+            } ?>
+        </ul>
+    </div>
     
-    <ul class="color-palletes rgb">
-        <li class="c1"></li>
-        <li class="c2"></li>
-        <li class="c3"></li>
-    </ul>
+    <div class="demo-description">
+        <h2>Readable Gray Scale Variables</h2>
+    </div>
+    <div class="demo-block">
+        <ul class="color-palletes short-code-gray-scale">
+            <?php foreach ($readable_gray_scale as $key=>$color) {
+                echo '<li title="'.$color.'" class="c'.($key + 1).'"><p>'.$color.'</p></li>';
+            } ?>
+        </ul>
+    </div>
     
-    <ul class="color-palletes cmyk">
-        <li class="c1"></li>
-        <li class="c2"></li>
-        <li class="c3"></li>
-        <li class="c4"></li>
-    </ul>
+    <div class="demo-description">
+        <h2>RGB Variables</h2>
+    </div>
+    <div class="demo-block">
+        <ul class="color-palletes rgb">
+            <?php foreach ($rgb as $key=>$color) {
+                echo '<li title="'.$color.'" class="c'.($key + 1).'"><p>'.$color.'</p></li>';
+            } ?>
+        </ul>
+    </div>
+    
+    <div class="demo-description">
+        <h2>CMYK Variables</h2>
+    </div>
+    <div class="demo-block">
+        <ul class="color-palletes cmyk">
+            <?php foreach ($cmyk as $key=>$color) {
+                echo '<li title="'.$color.'" class="c'.($key + 1).'"><p>'.$color.'</p></li>';
+            } ?>
+        </ul>
+    </div>
 
 </div><!-- .demo -->
