7d678511eddf17a436aca4d69bf73c942ad8ba40
Sebastian Nitu <me@sebnitu.com>
2013-04-23 00:31:45 -0700

    Reduced a lot of unnecessary complexity from the grid system. It's a lot simpler now with less variables to set. Woot

--- docs/inc/footer.php
+++ docs/inc/footer.php
@@ -10,6 +10,7 @@
 <script>!window.jQuery && document.write(unescape('%3Cscript src="assets/js/libs/jquery.min.js"%3E%3C/script%3E'))</script>
 
 <!-- BaseWeb jQuery Plugins -->
+<script src="assets/js/baseweb.js"></script>
 <script src="assets/js/baseweb.mobilemenus.js"></script>
 <script src="assets/js/baseweb.modals.js"></script>
 <script src="assets/js/baseweb.toggles.js"></script>
