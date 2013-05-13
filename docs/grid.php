5e448266f978d41c45fd14eb1c9f8fcc24543f1e
Sebastian Nitu <me@sebnitu.com>
2013-05-12 21:31:10 -0700

    Added the semantic grid mixins

--- docs/grid.php
+++ docs/grid.php
@@ -47,3 +47,55 @@
 <div class="row">
     <div class="col col-12"><p>.col-12</p></div>
 </div><!-- .row -->
+
+<hr>
+
+<div class="test-row">
+    <div class="test-col test-col-1"><p>.</p></div>
+    <div class="test-col test-col-1"><p>.</p></div>
+    <div class="test-col test-col-1"><p>.</p></div>
+    <div class="test-col test-col-1"><p>.</p></div>
+    <div class="test-col test-col-1"><p>.</p></div>
+    <div class="test-col test-col-1"><p>.</p></div>
+    <div class="test-col test-col-1"><p>.</p></div>
+    <div class="test-col test-col-1"><p>.</p></div>
+    <div class="test-col test-col-1"><p>.</p></div>
+    <div class="test-col test-col-1"><p>.</p></div>
+    <div class="test-col test-col-1"><p>.</p></div>
+    <div class="test-col test-col-1"><p>.</p></div>
+</div><!-- .row -->
+
+<div class="test-row">
+    <div class="test-col test-col-2"><p>.col-2</p></div>
+    <div class="test-col test-col-2"><p>.col-2</p></div>
+    <div class="test-col test-col-2"><p>.col-2</p></div>
+    <div class="test-col test-col-2"><p>.col-2</p></div>
+    <div class="test-col test-col-2"><p>.col-2</p></div>
+    <div class="test-col test-col-2"><p>.col-2</p></div>
+</div><!-- .row -->
+
+<div class="test-row">
+    <div class="test-col test-col-3"><p>.col-3</p></div>
+    <div class="test-col test-col-3"><p>.col-3</p></div>
+    <div class="test-col test-col-3"><p>.col-3</p></div>
+    <div class="test-col test-col-3"><p>.col-3</p></div>
+</div><!-- .row -->
+
+<div class="test-row">
+    <div class="test-col test-col-4"><p>.col-4</p></div>
+    <div class="test-col test-col-4"><p>.col-4</p></div>
+    <div class="test-col test-col-4"><p>.col-4</p></div>
+</div><!-- .row -->
+
+<div class="test-row">
+    <div class="test-col test-col-6">
+        <p>.col-6</p>
+    </div>
+    <div class="test-col test-col-6">
+        <p>.col-6</p>
+    </div>
+</div><!-- .row -->
+
+<div class="test-row">
+    <div class="test-col test-col-12"><p>.col-12</p></div>
+</div><!-- .row -->
