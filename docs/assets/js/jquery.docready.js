d977d46a2b79ffaf26b4ad6f24ac6486db2aa1b5
Sebastian Nitu <nitu.seb@gmail.com>
2016-12-26 17:15:11 -0800

    Added tabs base styles and JavaScript and started the tabs page.

--- docs/assets/js/jquery.docready.js
+++ docs/assets/js/jquery.docready.js
@@ -25,8 +25,9 @@
     });
 
     /**
-     * Bind the click event to .dropdown-trigger
+     * @Dropdowns
      */
+    // Bind the click event to .dropdown-trigger
     $('.dropdown-trigger.on-click').click(function(e) {
 
       // Is the dropdown already active?
@@ -47,9 +48,7 @@
       e.stopPropagation();
     });
 
-    /**
-     * Bind the click event to .dropdown
-     */
+    // Bind the click event to .dropdown
     $('.dropdown-trigger.on-click .dropdown').click(function(e) {
 
       // Hide all dropdowns that are click activated
@@ -66,15 +65,72 @@
     });
 
     /**
-     * Sticky Element
+     * @Tabs
+     */
+    $('.tabs-nav').each(function(e) {
+
+      // Save this
+      var $this = $(this);
+
+      // Save our tabs content
+      var tabs_content = $(this).parents('.tabs').find('.tabs-content');
+      var has_content = tabs_content.length;
+
+      // Check our other tabs content method if one wasn't found yet
+      if (!has_content) {
+        // Check if we have a linked content data attribute
+        tabs_content = $(this).attr('data-content');
+        if (tabs_content) {
+          // Save our tabs content
+          tabs_content = $('#' + tabs_content);
+          // Set has_content to true
+          if (tabs_content.length) {
+            has_content = 1;
+          }
+        } else {
+          console.log('Tabs content does not exist!');
+        }
+      }
+
+      // Add click event to tab links
+      $(this).find('a').click(function(e) {
+        // Check if item is already active or not
+        var is_active = $(this).parents('li').hasClass('active');
+
+        if (!is_active) {
+          // Remove active class from all children nav items
+          $this.find('li').removeClass('active');
+          // Add active class to currently selected item
+          $(this).parents('li').addClass('active');
+
+          // Check if tabs-nav has an associated content block
+          if (has_content) {
+            // Hide current active content
+            tabs_content.find('.tab-panel').removeClass('active');
+            // Show new active content
+            var target = $(this).attr('href');
+            $(target).addClass('active');
+          } else {
+            console.log('Tabs content does not exist!');
+          }
+        }
+
+        // Stop the default behavior
+        return false;
+      });
+
+    });
+
+    /**
+     * @Sticky Element
      */
     $('.sticky').theiaStickySidebar({
       containerSelector : '.row',
       additionalMarginTop : 0
     });
-    
+
     /**
-     * Navigation Toggle
+     * @Navigation Toggle
      */
     $('.widget-menu .toggle').click(function() {
       $(this).parent().toggleClass('active');
--- docs/docs/blocks/tabs.md
+++ docs/docs/blocks/tabs.md
@@ -7,11 +7,48 @@ order: 5
 Tabs are a navigation type component that gives a layered feel to a document. It usually will show and hide content without linking to a separate page entirely.
 
 <div class="tabs">
-  <ul>
-    <li><a href="#">Some Item</a></li>
-    <li><a href="#">Some Item</a></li>
-    <li><a href="#">Some Item</a></li>
-    <li><a href="#">Some Item</a></li>
-    <li><a href="#">Some Item</a></li>
-  </ul>
+  <nav class="tabs-nav">
+    <ul>
+      <li><a href="#tab-panel-1">Tab 1</a></li>
+      <li class="active"><a href="#tab-panel-2">Tab 2</a></li>
+      <li><a href="#tab-panel-3">Tab 3</a></li>
+      <li><a href="#tab-panel-4">Tab 4</a></li>
+      <li><a href="#tab-panel-5">Tab 5</a></li>
+    </ul>
+  </nav>
+  <div class="tabs-content">
+    <section id="tab-panel-1" class="tab-panel"><p>Tab Content 1</p></section>
+    <section id="tab-panel-2" class="tab-panel active"><p>Tab Content 2</p></section>
+    <section id="tab-panel-3" class="tab-panel"><p>Tab Content 3</p></section>
+    <section id="tab-panel-4" class="tab-panel"><p>Tab Content 4</p></section>
+    <section id="tab-panel-5" class="tab-panel"><p>Tab Content 5</p></section>
+  </div>
 </div>
+
+<nav class="tabs-nav" data-content="tabs-content-id">
+  <ul>
+    <li><a href="#tab-panel-6">Tab 1</a></li>
+    <li class="active"><a href="#tab-panel-7">Tab 2</a></li>
+    <li><a href="#tab-panel-8">Tab 3</a></li>
+    <li><a href="#tab-panel-9">Tab 4</a></li>
+    <li><a href="#tab-panel-10">Tab 5</a></li>
+  </ul>
+</nav>
+
+<div id="tabs-content-id" class="tabs-content">
+  <section id="tab-panel-6" class="tab-panel"><p>Tab Content 1</p></section>
+  <section id="tab-panel-7" class="tab-panel active"><p>Tab Content 2</p></section>
+  <section id="tab-panel-8" class="tab-panel"><p>Tab Content 3</p></section>
+  <section id="tab-panel-9" class="tab-panel"><p>Tab Content 4</p></section>
+  <section id="tab-panel-10" class="tab-panel"><p>Tab Content 5</p></section>
+</div>
+
+<nav class="tabs-nav">
+  <ul>
+    <li><a href="#tab-panel-6">Tab 1</a></li>
+    <li class="active"><a href="#tab-panel-7">Tab 2</a></li>
+    <li><a href="#tab-panel-8">Tab 3</a></li>
+    <li><a href="#tab-panel-9">Tab 4</a></li>
+    <li><a href="#tab-panel-10">Tab 5</a></li>
+  </ul>
+</nav>
