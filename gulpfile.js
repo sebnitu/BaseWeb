80aad0c1fab7d7098d515a3bbb16696496cec9f5
sebnitu <me@sebnitu.com>
2017-01-26 20:12:45 -0800

    Removed all the prefixes from mixins and finsihed building the source to distribution builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 // BaseWeb's Gulpfile
 // Gulp.js configuration
 var
-  // modules
+  // Require Modules
+  // Gulp
   gulp = require('gulp'),
+  rename = require('gulp-rename'),
+  merge = require('merge-stream'),
+  // CSS
   sass = require('gulp-sass'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  mqpacker = require('css-mqpacker'),
+  cssnano = require('cssnano'),
+  // JS
+  concat = require('gulp-concat'),
+  deporder = require('gulp-deporder'),
+  stripdebug = require('gulp-strip-debug'),
+  uglify = require('gulp-uglify'),
 
   // folders
   folder = {
@@ -14,16 +27,54 @@ var
 ;
 
 // CSS processing
-gulp.task('css', /*['img'],*/ function() {
+// Output expanded and minified CSS files + plugins
+gulp.task('css', function() {
 
-  var out = folder.dest + 'css/';
-
-  return gulp.src(folder.src + 'scss/**/*.scss')
-    .pipe(sass({
+  var
+    src = folder.src + 'scss/baseweb.scss',
+    dest = folder.dest + 'css/',
+    sassOpts = {
       outputStyle: 'expanded',
       precision: 3
-    })
-    .on('error', sass.logError))
-    .pipe(gulp.dest(out));
+    },
+    postcssOpts = [
+      autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
+      mqpacker
+    ],
+    css = gulp.src(src)
+      .pipe(sass(sassOpts)
+      .on('error', sass.logError))
+      .pipe(postcss(postcssOpts))
+      .pipe(gulp.dest(dest)),
+    cssmin = gulp.src(src)
+      .pipe(sass(sassOpts)
+      .on('error', sass.logError))
+      .pipe(postcss(postcssOpts))
+      .pipe(postcss([cssnano]))
+      .pipe(rename('baseweb.min.css'))
+      .pipe(gulp.dest(dest));
+
+  return merge(css, cssmin);
+});
+
+// JavaScript processing
+// Output expanded and minified JS files + plugins
+gulp.task('js', function() {
+
+  var
+    src = folder.src + 'js/**/*',
+    dest = folder.dest + 'js/',
+    js = gulp.src(src)
+      .pipe(deporder())
+      .pipe(concat('baseweb.js'))
+      .pipe(gulp.dest(dest)),
+    jsmin = gulp.src(src)
+      .pipe(deporder())
+      .pipe(concat('baseweb.min.js'))
+      .pipe(stripdebug())
+      .pipe(uglify())
+      .pipe(gulp.dest(dest));
+
+  return merge(js, jsmin);
 
 });
