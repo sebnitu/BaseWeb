27221898e27dbcc8803c720ca09087551fe07f83
sebnitu <me@sebnitu.com>
2017-03-04 13:49:25 -0800

    Finished writing the docs for off-canvas component [Progress #56]

--- docs/dist/js/scripts.js
+++ docs/dist/js/scripts.js
@@ -517,59 +517,66 @@ function getContrastYIQ(hexcolor) {
     /**
      * @Off Canvas
      */
-    $('.oc-trigger').each(function () {
-      var
-        $this = $(this),
-        $wrap = $(this).closest('.oc-wrap'),
-        $aside = $wrap.find('.oc-aside'),
-        target = $this.attr('data-target'),
-        reset = $wrap.attr('class'),
-        close = function() {
-          // Remove active class
-          $wrap.removeClass('oc-active');
-          // Remove delay class after the set transition duration
-          setTimeout( function() {
-            $wrap.removeClass('oc-delay');
-          }, 525 );
-        }
-      ;
+    function off_canvas() {
+      $('.oc-trigger').each(function () {
+        var
+          $this = $(this),
+          $wrap = $this.closest('.oc-wrap'),
+          $aside = $wrap.find('.oc-aside'),
+          target = $this.attr('data-target'),
+          reset = 'oc-wrap',
+          is_active = false,
+          close = function() {
+            // Remove active class
+            $wrap.removeClass('oc-active');
+            // Remove delay class after the set transition duration
+            setTimeout( function() {
+              $wrap.removeClass('oc-delay');
+            }, 500 );
+          }
+        ;
 
-      // Button click event
-      $this.click(function(e) {
-        // Check if it's a close button
-        if($(this).hasClass('close')) {
+        // Button click event
+        $this.click(function(e) {
+          is_active = $wrap.hasClass('oc-active');
+
+          // Check if it's a close button or if off-canvas is already active
+          if(!target || is_active) {
+            // Close off-canvas content
+            close();
+          } else {
+            // Reset container class
+            $wrap.attr('class', reset);
+          }
+          // Add target class
+          if(target && !is_active) {
+            $wrap.addClass(target);
+            // Add active and delay classes after a slight delay
+    				setTimeout( function() {
+              $wrap.addClass('oc-active oc-delay');
+    				}, 25 );
+          }
+
+          // Stop the default behavior
+          return false;
+        });
+
+        // Aside click event
+        $aside.click(function(e) {
+          // Stop the click propogation from bubbling down to the container
+          e.stopPropagation();
+        });
+
+        // Container click event
+        $wrap.click(function(e) {
           // Close off-canvas content
           close();
-        } else {
-          // Reset container class
-          $wrap.attr('class', reset);
-        }
-        // Add target class
-        if(target) {
-          $wrap.addClass(target);
-          // Add active and delay classes after a slight delay
-  				setTimeout( function() {
-            $wrap.addClass('oc-active oc-delay');
-  				}, 25 );
-        }
+        });
 
-        // Stop the default behavior
-        return false;
       });
+    }
 
-      // Aside click event
-      $aside.click(function(e) {
-        // Stop the click propogation from bubbling down to the container
-        e.stopPropagation();
-      });
-
-      // Container click event
-      $wrap.click(function(e) {
-        // Close off-canvas content
-        close();
-      });
-
-    });
+    off_canvas();
 
     /**
      * @Docs Interface
@@ -599,6 +606,30 @@ function getContrastYIQ(hexcolor) {
       var bg = $(this).css('backgroundColor');
       var text = getContrastYIQ(rgb2hex(bg));
       $(this).addClass(text);
+    });
+
+    // Switch off-canvas class
+    $('.form-off-canvas-transitions select').change(function () {
+      var
+        $this = $(this),
+        oc_effect = $('#oc-effect').val(),
+        oc_position = $('#oc-position').val(),
+        oc_class = oc_effect + '-' + oc_position,
+        $oc_transition_value = $('#oc-transition-value'),
+        $oc_trigger = $('#oc-trigger-sample'),
+        $oc_aside = $this.closest('.oc-wrap').find('.oc-aside')
+      ;
+
+      $oc_transition_value.val(oc_class);
+      $oc_trigger.attr('data-target', oc_class);
+      $oc_aside.attr('class', 'oc-aside ' + oc_class);
+
+      off_canvas();
+    });
+
+    // Select on focus
+    $('.form-off-canvas-transitions input[readonly]').focus(function() {
+      $(this).select();
     });
 
   });
